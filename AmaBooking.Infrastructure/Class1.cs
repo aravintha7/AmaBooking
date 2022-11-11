335ec2a147cd7136299f7330afa6e6ec42d1aba3
aravinth_a <aravinth.a@amadeus.com>
2022-11-11 15:34:43 +0530

    create solution

---
+++ AmaBooking.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AmaBooking.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/AmaBooking.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AmaBooking.Domain
+{
+    public class Class1
+    {
+    }
+}
